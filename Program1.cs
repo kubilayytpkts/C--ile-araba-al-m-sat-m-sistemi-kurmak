0d616fcc5c4e446b8bcaec506082c9259cb6ce03
kubilayytpkts <119957098+kubilayytpkts@users.noreply.github.com>
2023-01-09 20:03:59 +0300

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            araba a1 = new araba();
+
+            a1.bilgiler();
+        
+            Console.WriteLine("Teklif Vermek ister misiniz ? E/H");
+           
+            String kullanıcıEH = Console.ReadLine();
+            Console.Clear();
+
+            int kullanıcıteklif;
+            int genelfiyat = 0; 
+           
+
+            do
+            {
+                Console.Write("Lütfen Vermek istediğiniz indirim teklifini giriniz :");
+               
+
+                kullanıcıteklif = int.Parse(Console.ReadLine());
+                if(kullanıcıteklif<a1.MAXindirim)
+                {
+                    Console.Clear();
+                    System.Threading.Thread.Sleep(500);
+
+                    Console.WriteLine("Sayın Müşterimiz vermiş olduğunuz teklif Tarafımızca kabul edilmiştir...");
+                    genelfiyat =a1.SatışFiyatı-kullanıcıteklif;
+                    Console.WriteLine("{0} TL ile Aracınızı indirimli bir şekilde alabilirsiniz Lütfen en yakın Bayimiz İle İletişime geçiniz Saygılar...",genelfiyat);
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Sayın Müşterimiz girmiş olduğunuz Fiyat Kabul edilememiştir lütfen tekrar Fiyat veriniz");
+                    
+                    
+                }
+                Console.ReadLine();
+                
+
+            } while (kullanıcıEH != "E");
+            
+        }
+    }
+}
---
+++ program1Class1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class araba
+    {
+        public string marka="Audi";
+        public string markaModel="H500";
+        public int ModelYıl = 2000;
+        public int Km=10000;
+        public string yakıtTipi = "Benzinli";
+        public int ArabaVitesi = 6;
+        private int AlışFiyatı=8500;
+        public int SatışFiyatı=15000;
+        public int MAXindirim = 2500;
+        public int Fiyat = 15000;
+
+        public araba()
+        {
+        
+        }
+
+        public araba(string _Marka, int _ModelYıl)
+        {
+            marka = _Marka;
+            ModelYıl = _ModelYıl;
+        }
+
+        public araba(string _Marka,string _Markamodel,int _ModelYıl)
+        {
+            marka =_Marka;
+            markaModel = _Markamodel;
+            ModelYıl = _ModelYıl;
+        }
+
+        public araba(string _Marka,string _Markamodel,int _ModelYıl,int _km)
+        {
+            marka = _Marka;
+            markaModel = _Markamodel;
+            ModelYıl = _ModelYıl;
+            Km = _km;
+        }
+
+
+        
+        public void bilgiler()
+        {
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Araç Markası:{0}", marka);
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Araç Modeli:{0}",markaModel);
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Araç ModelYılı:{0}",ModelYıl);
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Araç km'si:{0}",Km);
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Araç Yakıt tipi :{0}",yakıtTipi);
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Araç Vites Sayısı:{0}",ArabaVitesi);
+            Console.WriteLine("***************************");
+            Console.WriteLine("Araç Satış Fiyatı:{0}",SatışFiyatı);
+            Console.WriteLine("***************************");
+            
+
+
+
+
+
+        }
+
+
+
+    }
+}
